77be5f9317c1fc1fa382338c4dacd233f4dc17cd
liorlevinus-design <liorlevinus@gmail.com>
2026-09-15 16:34:01 +0300

    Add command-surface selection cycle tests

---
+++ tests/DmxConsole.Application.Tests/CommandSurface/SelectionCycleComposerTests.cs
@@ -0,0 +1,60 @@
+using DmxConsole.Application.CommandSurface;
+
+namespace DmxConsole.Application.Tests.CommandSurface;
+
+public class SelectionCycleComposerTests
+{
+    [Fact]
+    public void BareNumbers_DefaultToFixtureContext()
+    {
+        var (context, dispatcher, _) = TestFixtures.BuildConsole(fixtureCount: 5);
+        var composer = new CommandComposer(context);
+
+        composer.Push(CommandToken.Number(1));
+        composer.Push(CommandToken.Simple(CommandTokenKind.Thru));
+        composer.Push(CommandToken.Number(3));
+        var final = composer.Push(CommandToken.Simple(CommandTokenKind.Enter));
+
+        Assert.True(final.IsComplete);
+        Assert.Equal("1 THRU 3", final.PreviewText.ToUpperInvariant());
+        Assert.NotNull(final.ReadyOperation);
+
+        dispatcher.Dispatch(final.ReadyOperation!);
+        Assert.Equal(new[] { 1, 2, 3 }, context.Selection.Items.Select(f => f.Number).ToArray());
+    }
+
+    [Fact]
+    public void AppendMode_AddsWithoutTogglingExistingSelectionOff()
+    {
+        var (context, dispatcher, _) = TestFixtures.BuildConsole(fixtureCount: 3);
+        context.Selection.Add(context.Patch.Fixtures.First(f => f.Number == 1));
+
+        var composer = new CommandComposer(context) { ReplaceSelectionOnResolve = false };
+        composer.Push(CommandToken.Number(1));
+        composer.Push(CommandToken.Number(2));
+        var final = composer.Push(CommandToken.Simple(CommandTokenKind.Enter));
+
+        Assert.True(final.IsComplete);
+        dispatcher.Dispatch(final.ReadyOperation!);
+
+        Assert.Equal(new[] { 1, 2 }, context.Selection.Items.Select(f => f.Number).ToArray());
+    }
+
+    [Fact]
+    public void At_ClosesSelectionCycle_SelectionOnlyDoesNot()
+    {
+        var (context, _, _) = TestFixtures.BuildConsole(fixtureCount: 2);
+
+        var selectionOnly = new CommandComposer(context);
+        selectionOnly.Push(CommandToken.Number(1));
+        var selected = selectionOnly.Push(CommandToken.Simple(CommandTokenKind.Enter));
+        Assert.False(selected.EndsSelectionCycle);
+
+        var withAt = new CommandComposer(context);
+        withAt.Push(CommandToken.Number(1));
+        withAt.Push(CommandToken.Simple(CommandTokenKind.At));
+        withAt.Push(CommandToken.Number(50));
+        var executed = withAt.Push(CommandToken.Simple(CommandTokenKind.Enter));
+        Assert.True(executed.EndsSelectionCycle);
+    }
+}
